22b22080c8b30e8d3f5ad7f0572f14fb8a9e5b70
eman-m0hamed <emanmohamed73359@gmail.com>
2025-08-26 02:27:23 +0300

    edit updateRequest for user update to enable not changing username or email

--- app/Http/Requests/User/UpdateRequest.php
+++ app/Http/Requests/User/UpdateRequest.php
@@ -14,8 +14,9 @@ class UpdateRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'user.username' => 'sometimes|string|max:50|unique:users,username',
-            'user.email' => 'sometimes|email|max:255|unique:users,email',
+            'user.username' => 'sometimes|string|max:50|unique:users,username,' . $this->user()->id,
+
+            'user.email' => 'sometimes|email|max:255|unique:users,email,' . $this->user()->id,
             'user.password' => 'sometimes',
             'user.image' => 'sometimes|url',
             'user.bio' => 'sometimes|string|max:2048'
