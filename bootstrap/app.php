a5a54ddeebe4d1711106eb54b78908b6fb8d834c
eman-m0hamed <emanmohamed73359@gmail.com>
2025-08-26 02:29:36 +0300

    render ApiExceptionTrait in my app

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,8 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
+use App\Traits\ApiExceptionTrait;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -16,5 +18,26 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
-    })->create();
+        $exceptions->shouldRenderJsonWhen(function (Request $request, \Throwable $e) {
+            // Force JSON for routes starting with 'api/'
+            if ($request->is('api/*')) {
+                return true;
+            }
+            // Fall back to default behavior
+            return $request->expectsJson();
+        });
+
+
+    /*
+    * This code registers a global exception renderer for your application.
+    * It uses the `$exceptions->render()` method to intercept all exceptions
+    * and pass them through a custom handler defined in `ApiExceptionTrait`.
+    */
+    $exceptions->render(function (\Throwable $e, Request $request) {
+            $trait = new class {
+                use ApiExceptionTrait;
+            };
+            return $trait->renderApi($request, $e);
+        });
+    })
+    ->create();
