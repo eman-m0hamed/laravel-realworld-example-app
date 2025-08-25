8b0bd6578ca6450c8059043c956aed6b260a3b06
eman-m0hamed <emanmohamed73359@gmail.com>
2025-08-25 03:16:53 +0300

    edit files to be suitable to v11, remove unwanted files like kernal and middlewares folder, all providers except appServiceProvider, edit app file and add providers file in bootstrap folder

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,10 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Http\Request;
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -24,5 +28,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+        RateLimiter::for('api', function (Request $request) {
+            return Limit::perMinute(60)->by(optional($request->user())->id ?: $request->ip());
+        });
     }
 }
